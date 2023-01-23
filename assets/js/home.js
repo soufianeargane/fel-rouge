c5a2b863492b68b149bf0354420f648e01b3adc1
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2023-01-23 15:02:09 +0100

    still working on the navbar

---
+++ assets/js/home.js
@@ -0,0 +1,5 @@
+// toggle navbar
+document.getElementById("show-navbar").addEventListener("click", function () {
+    console.log("show navbar");
+    document.getElementById("navbar").classList.toggle("show");
+});
