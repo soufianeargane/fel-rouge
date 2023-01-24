6078e2ed344930e335c54f0e4ec1b6afc695e547
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2023-01-24 21:04:26 +0100

    added the click on the window to toggle navbar

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,23 @@
-// toggle navbar
-document.getElementById("show-navbar").addEventListener("click", function () {
-    console.log("show navbar");
-    document.getElementById("navbar").classList.toggle("show");
+// toggle navba
+
+document.addEventListener("click", function (e) {
+    if (
+        e.target.classList.contains("icon__nav") ||
+        e.target.classList.contains("bi-list")
+    ) {
+        document.getElementById("navbar").classList.toggle("show");
+    } else if (!e.target.classList.contains("navv")) {
+        document.getElementById("navbar").classList.remove("show");
+    }
 });
+// document.getElementById("show-navbar").addEventListener("click", function () {
+//     console.log("show navbar");
+//     document.getElementById("navbar").classList.toggle("show");
+// });
+
+// hide navbar when click outside if it contains class show
+// document.addEventListener("click", function () {
+//     if (document.getElementById("navbar").classList.contains("show")) {
+//         document.getElementById("navbar").classList.remove("show");
+//     }
+// });
