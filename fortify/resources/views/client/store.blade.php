941b4a824dbdbdbcbf2e66d1adb6915a32c757b7
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2023-04-11 14:14:09 +0000

    finished input type search

--- fortify/resources/views/client/store.blade.php
+++ fortify/resources/views/client/store.blade.php
@@ -67,6 +67,10 @@
             background-color: #7cbda0;
             color: #000;
         }
+        .light-color{
+            background-color: #7cbda0;
+            color: #000;
+        }
 
 
     </style>
@@ -90,11 +94,18 @@
                     </select>
 
                     <button
-                    class="bg-red-300 rounded p-1 mt-2 search-btn"
+                    class="light-color rounded py-1 px-3 mt-2 search-btn"
                     >
                         Filter
                     </button>
                 </div>
+            </div>
+            <div class="mt-12 p-1">
+                <input type="search"
+                placeholder="Search for a store"
+                id="search_input_store"
+                class="w-full border border-gray-300 rounded px-3 py-1  focus:outline-none focus:border-gray-400 text-xs"
+                >
             </div>
 
         </div>
@@ -182,12 +193,12 @@
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <h5 class="mb-2 store-title text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                             ${store.title}
                         </h5>
                     </a>
                     {{-- <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p> --}}
-                    <a href="store/${store.id}/details" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <a href="store/${store.id}/details" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white light-color  rounded-lg hover: focus:ring-4 focus:outline-none focus:ring-blue-300 dark: dark:hover: dark:focus:ring-blue-800">
                             Products
                             <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                         </a>
@@ -261,4 +272,18 @@
     });
 
     // style="background-size: cover; background-position: center; background-image: url('img/store/${store.image}'); height: 250px; width: 100%; background-repeat: no-repeat;"
+    $(document).ready(function(){
+        $('#search_input_store').on('keyup', function(){
+            var value = $(this).val();
+            // console.log(value);
+            // display none all the stores that don't match the class store-title in the heading
+            $('.store-title').each(function(){
+                if($(this).text().toLowerCase().indexOf(value.toLowerCase()) == -1){
+                    $(this).parent().parent().parent().hide();
+                } else {
+                    $(this).parent().parent().parent().show();
+                }
+            })
+        })
+    })
 </script>
