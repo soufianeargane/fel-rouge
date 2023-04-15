9dd02e39e4636e662ddcdcd059331d5e608f6c8e
soufianeargane <112890451+soufianeargane@users.noreply.github.com>
2023-04-15 15:58:26 +0000

    added chartjs

--- fortify/app/Http/Controllers/OwnerController.php
+++ fortify/app/Http/Controllers/OwnerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -11,28 +12,46 @@ class OwnerController extends Controller
     public function index()
     {
         $user_id = auth()->user()->id;
-        $stores = DB::table('stores')->where('user_id', $user_id)->where('status',1)->get();
-        dd($stores);
-        // $storeId = ; 
-
-        $store = Store::with('ratings.user')->where('id', $storeId)->first();
+        $store = Store::where('user_id', $user_id)->where('status', 1)->first();
+        // dd($store);
+        $products = $store->products;
+        $orders = $store->orders ;
 
         if ($store) {
-            $commentsData = [];
-            foreach ($store->ratings as $rating) {
-                $commentsData[] = [
-                    'id' => $rating->id,
-                    'rating' => $rating->rating,
-                    'comment' => $rating->comment,
-                    'created_at' => $rating->created_at->diffForHumans(),
-                    'user_name' => $rating->user->name,
-                    'store_name' => $store->title,
-                ];
+            $averageRating = $store->averageRating;
+            $comments = Store::with('ratings.user')->where('id', $store->id)->first();
+
+            if ($comments) {
+                $commentsData = [];
+                foreach ($store->ratings as $rating) {
+                    $commentsData[] = [
+                        'rating' => $rating->rating,
+                        'comment' => $rating->comment,
+                        'created_at' => $rating->created_at->diffForHumans(),
+                        'user_name' => $rating->user->name,
+                    ];
+                }
             }
-        } else {
-            // Store not found, handle this case as needed
         }
 
-        return view('owner.shop');
+        // get status of orders
+        $orders = $store->orders ;
+        $statusCounts = [
+            'pending' => 0,
+            'accepted' => 0,
+            'rejected' => 0,
+        ];
+        foreach ($orders as $order) {
+            if ($order->status == 0) {
+                $statusCounts['pending']++;
+            } elseif ($order->status == 1) {
+                $statusCounts['accepted']++;
+            } elseif ($order->status == 2) {
+                $statusCounts['rejected']++;
+            }
+        }
+
+
+        return view('owner.shop', compact('store', 'commentsData', 'averageRating', 'statusCounts'));
     }
 }
--- fortify/resources/views/owner/shop.blade.php
+++ fortify/resources/views/owner/shop.blade.php
@@ -1,3 +1,35 @@
 <x-owner>
-    sooo dashboard 
+    sooo dashboard
+    <div class="w-1/2">
+    <canvas id="doughnutChart"></canvas>
+    </div>
+
 </x-owner>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const ctx = document.getElementById('doughnutChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Pending', 'Accepted', 'Rejected'],
+                datasets: [{
+                    data: [{{ $statusCounts['pending'] }}, {{ $statusCounts['accepted'] }}, {{ $statusCounts['rejected'] }}],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+            }
+        });
+
+    </script>
